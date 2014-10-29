0a01118601acbc634feefc5c9cf72bbbb1e21bf8
Sebastian Fernstedt <sf222em@student.lnu.se>
2014-10-29 17:18:26 +0100

    Started coding

---
+++ FiledRecipes/FiledRecipes/Views/RecipeView.cs
@@ -0,0 +1,25 @@
+﻿using FiledRecipes.Domain;
+using FiledRecipes.App.Mvp;
+using FiledRecipes.Properties;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FiledRecipes.Views
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class RecipeView : ViewBase, IRecipeView
+    {
+//NEW METHODS
+        public void Show(IRecipe recipe)
+        {
+            throw new NotImplementedException();
+        }
+         public void Show(IEnumerable<IRecipe> recipes)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
